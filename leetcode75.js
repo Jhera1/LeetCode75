2eb16d06249b6ac78eb8810c1feb3031396847c9
Justin Armstrong <armstrongjustin034@gmail.com>
2025-02-01 19:31:03 -0800

    completed canPlaceFlowers line 69

--- leetcode75.js
+++ leetcode75.js
@@ -1,12 +1,10 @@
-// const add = (n1, n2) => n1 + n2
-// console.log(add(2, 2))
 
+
+// 1768. Merge Strings Alternately
 
 // You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
 // Return the merged string.
-
- 
 
 // Example 1:
 
@@ -44,3 +42,39 @@ const mergeAlternately = (str1, str2) => {
 }
 console.log(mergeAlternately('stop', 'rant'))
 
+
+
+
+
+
+// 605. Can Place Flowers
+
+
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+
+const canPlaceFlowers = (flowerbed, n) => {
+    let flowersPLanted = 0   
+   for (let i = 0; i < flowerbed.length; i++) {
+    if ((i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0))  {
+        flowerbed[i] = 1
+        flowersPLanted++
+        i++
+    }
+   }
+    return flowersPLanted >= n    
+}
+console.log(canPlaceFlowers([1, 0, 0, 0, 1,], 1))
